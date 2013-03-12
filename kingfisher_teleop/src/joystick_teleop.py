365eddfd643cf910795e4042dd7e72c3ccda4c20
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-03-12 15:07:51 -0400

    Add turbo mode to standard joystick.

--- kingfisher_teleop/src/joystick_teleop.py
+++ kingfisher_teleop/src/joystick_teleop.py
@@ -12,8 +12,11 @@ class Teleop:
         rospy.init_node('clearpath_teleop')
 
         self.turn_scale = rospy.get_param('~turn_scale')
-        self.drive_scale = rospy.get_param('~drive_scale')
+        self.drive_scale = rospy.get_param('~drive_scale', 0.7)
+        self.turbo_drive_scale = rospy.get_param('~turbo_drive_scale', 1.0)
+
         self.deadman_button = rospy.get_param('~deadman_button', 0)
+        self.turbo_button = rospy.get_param('~turbo_button', 1)
 
         self.cmd_pub = rospy.Publisher('cmd_vel', Twist)
 
@@ -22,9 +25,15 @@ class Teleop:
     
     def callback(self, data):
         """ Receive joystick data, formulate Twist message. """
+        scale = 0
         if data.buttons[self.deadman_button] == 1:
+            scale = self.drive_scale
+        if data.buttons[self.turbo_button] == 1:
+            scale = self.turbo_drive_scale
+
+        if scale != 0:
             cmd = Twist()
-            cmd.linear.x = data.axes[1] * self.drive_scale
+            cmd.linear.x = data.axes[1] * scale
             cmd.angular.z = data.axes[0] * self.turn_scale
             self.cmd_pub.publish(cmd)
 
