093b01a70d37ff61efab62d5c996296000f25be3
Prasenjit Mukherjee <pmukherj@clearpathrobotics.com>
2013-06-21 17:31:39 -0400

    kingfisher teleop node modified to handle autonomy modes and to publish forces in raw teleop mode

--- kingfisher_teleop/src/joystick_teleop.py
+++ kingfisher_teleop/src/joystick_teleop.py
@@ -4,26 +4,37 @@ import roslib; roslib.load_manifest('kingfisher_teleop')
 import rospy
 
 from sensor_msgs.msg import Joy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist,Wrench
 
 
 class Teleop:
     def __init__(self):
         rospy.init_node('clearpath_teleop')
 
-        self.turn_scale = rospy.get_param('~turn_scale')
-        self.drive_scale = rospy.get_param('~drive_scale', 0.7)
-        self.turbo_drive_scale = rospy.get_param('~turbo_drive_scale', 1.0)
+        self.rotation_scale = rospy.get_param('~rotation_scale',0.5) #rad/s
+        self.fwd_vel_scale = rospy.get_param('~fwd_vel_scale', 75) #m/s
+        self.bck_vel_scale = rospy.get_param('~bck_vel_scale',40) #m/s
 
+        self.boat_width = 0.381
+        self.fwd_force_scale = rospy.get_param('~fwd_force_scale',75) #Newtons
+        self.bck_force_scale = rospy.get_param('~bck_force_scale',40) #Newtons
+        self.torque_scale = rospy.get_param('~torque_scale',self.bck_force_scale*2*self.boat_width) #N.m, max torque depends on max reverse thrust possible
+         
         self.deadman_button = rospy.get_param('~deadman_button', 0)
-        self.turbo_button = rospy.get_param('~turbo_button', 1)
+        #self.turbo_button = rospy.get_param('~turbo_button', 1)
+        self.closed_loop = rospy.get_param('~closed_loop',False)
+        self.autonomous_button = rospy.get_param('~autonomous_button',2)
+
+        self.autonomous = False
+        self.last_auto_button = 0
 
         self.max_age = None
         max_age_seconds = rospy.get_param('~max_age_seconds', None)
         if max_age_seconds:
             self.max_age = rospy.Duration.from_sec(max_age_seconds)
 
-        self.cmd_pub = rospy.Publisher('cmd_vel', Twist)
+        self.vel_pub = rospy.Publisher('cmd_vel', Twist)
+        self.wrench_pub = rospy.Publisher('cmd_wrench',Wrench)
 
         rospy.Subscriber("joy", Joy, self.callback)
         rospy.spin()
@@ -34,18 +45,39 @@ class Teleop:
         if self.max_age and data.header.stamp - rospy.Time.now() > self.max_age:
             # Joystick messages are too delayed. Discard them.
             return
+        
+        auto_button = data.buttons[self.autonomous_button]
+        if auto_button==1 and self.last_auto_button==0: #catch button transition
+            self.autonomous = not self.autonomous 
+            if (self.autonomous):
+                rospy.loginfo("Boat under closed loop control")
+            else:
+                rospy.loginfo("Boat in R/C mode")
+        self.last_auto_button = auto_button
 
-        scale = 0
-        if data.buttons[self.deadman_button] == 1:
-            scale = self.drive_scale
-        if data.buttons[self.turbo_button] == 1:
-            scale = self.turbo_drive_scale
+        if (self.autonomous): #command closed loop velocites on a twist message
+            scale = 0
+            if data.buttons[self.deadman_button] == 1:
+                if (data.axes[1] >= 0):
+                    scale = self.fwd_vel_scale 
+                else:
+                    scale = self.bck_vel_scale
+                cmd = Twist()
+                cmd.angular.z = -data.axes[0] * self.rotation_scale 
+                cmd.linear.x = data.axes[1] * scale
+                self.vel_pub.publish(cmd)
 
-        if scale != 0:
-            cmd = Twist()
-            cmd.linear.x = data.axes[1] * scale
-            cmd.angular.z = data.axes[0] * self.turn_scale
-            self.cmd_pub.publish(cmd)
+        else: # command open loop thrusters on a wrench message
+            scale = 0
+            if data.buttons[self.deadman_button] == 1:
+                if (data.axes[1] >= 0):
+                    scale = self.fwd_force_scale 
+                else:
+                    scale = self.bck_force_scale
+                cmd = Wrench()
+                cmd.torque.z = -data.axes[0] * self.torque_scale
+                cmd.force.x = data.axes[1]  * scale
+                self.wrench_pub.publish(cmd)
 
 
 if __name__ == "__main__": Teleop()
