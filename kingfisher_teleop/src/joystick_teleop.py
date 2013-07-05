2070c68e6227b9a8d6d8967c9bf8ff72597ecf19
Prasenjit Mukherjee <pmukherj@clearpathrobotics.com>
2013-07-05 18:15:36 -0400

    added heading hold to kingfisher teleop, holds the current heading

--- kingfisher_teleop/src/joystick_teleop.py
+++ kingfisher_teleop/src/joystick_teleop.py
@@ -3,8 +3,10 @@
 import roslib; roslib.load_manifest('kingfisher_teleop')
 import rospy
 
-from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Joy,Imu
 from geometry_msgs.msg import Twist,Wrench
+from kingfisher_msgs.msg import YawSpd, Helm
+from tf.transformations import euler_from_quaternion
 
 
 class Teleop:
@@ -12,8 +14,8 @@ class Teleop:
         rospy.init_node('clearpath_teleop')
 
         self.rotation_scale = rospy.get_param('~rotation_scale',0.5) #rad/s
-        self.fwd_vel_scale = rospy.get_param('~fwd_vel_scale', 75) #m/s
-        self.bck_vel_scale = rospy.get_param('~bck_vel_scale',40) #m/s
+        self.fwd_vel_scale = rospy.get_param('~fwd_vel_scale', 5) #m/s
+        self.bck_vel_scale = rospy.get_param('~bck_vel_scale',0.5) #m/s
 
         self.boat_width = 0.381
         self.fwd_force_scale = rospy.get_param('~fwd_force_scale',75) #Newtons
@@ -22,22 +24,44 @@ class Teleop:
          
         self.deadman_button = rospy.get_param('~deadman_button', 0)
         #self.turbo_button = rospy.get_param('~turbo_button', 1)
-        self.closed_loop = rospy.get_param('~closed_loop',False)
+        self.heading_hold_button = rospy.get_param('~heading_hold_button',3)
         self.autonomous_button = rospy.get_param('~autonomous_button',2)
 
         self.autonomous = False
         self.last_auto_button = 0
+
+        self.heading_hold = False
+        self.curr_heading=0
+        self.hold_heading = 0
+        self.last_heading_button = 0
 
         self.max_age = None
         max_age_seconds = rospy.get_param('~max_age_seconds', None)
         if max_age_seconds:
             self.max_age = rospy.Duration.from_sec(max_age_seconds)
 
-        self.vel_pub = rospy.Publisher('cmd_vel', Twist)
+        self.helm_pub = rospy.Publisher('cmd_helm', Helm)
         self.wrench_pub = rospy.Publisher('cmd_wrench',Wrench)
+        self.heading_pub = rospy.Publisher('cmd_yawspd',YawSpd)
+
 
         rospy.Subscriber("joy", Joy, self.callback)
+        rospy.Subscriber("imu/data_final",Imu,self.imu_callback) 
         rospy.spin()
+   
+    def output_mode(self):
+        if (self.heading_hold):
+            rospy.loginfo("Boat under heading hold")
+            self.hold_heading = self.curr_heading
+        elif (self.autonomous):
+            rospy.loginfo("Boat under helm control")
+        else:
+            rospy.loginfo("Boat in raw R/C mode")
+
+    def imu_callback(self,data):
+        q  = data.orientation
+        x,y,z = euler_from_quaternion([ getattr(q,f) for f in q.__slots__])
+        self.curr_heading = z
     
     def callback(self, data):
         """ Receive joystick data, formulate Twist message. """
@@ -47,26 +71,45 @@ class Teleop:
             return
         
         auto_button = data.buttons[self.autonomous_button]
-        if auto_button==1 and self.last_auto_button==0: #catch button transition
-            self.autonomous = not self.autonomous 
-            if (self.autonomous):
-                rospy.loginfo("Boat under closed loop control")
-            else:
-                rospy.loginfo("Boat in R/C mode")
-        self.last_auto_button = auto_button
+        heading_button = data.buttons[self.heading_hold_button]
 
-        if (self.autonomous): #command closed loop velocites on a twist message
+        #catch button transition
+        if heading_button==1 and self.last_heading_button==0:
+            self.heading_hold=not self.heading_hold
+            self.output_mode()
+        elif auto_button==1 and self.last_auto_button==0:
+            self.autonomous = not self.autonomous 
+            self.output_mode()
+        
+        self.last_auto_button = auto_button
+        self.last_heading_button = heading_button
+
+        if (self.heading_hold):
             scale = 0
             if data.buttons[self.deadman_button] == 1:
                 if (data.axes[1] >= 0):
                     scale = self.fwd_vel_scale 
                 else:
                     scale = self.bck_vel_scale
-                cmd = Twist()
-                cmd.angular.z = -data.axes[0] * self.rotation_scale 
-                cmd.linear.x = data.axes[1] * scale
-                self.vel_pub.publish(cmd)
+                cmd=YawSpd()
+                cmd.heading = self.hold_heading
+                cmd.speed = data.axes[1] * scale
+                self.heading_pub.publish(cmd)
 
+
+
+        elif (self.autonomous): #command closed loop yaw rate + thrust pct on helm message
+            if data.buttons[self.deadman_button] == 1:
+                #if (data.axes[1] >= 0):
+                #    scale = self.fwd_vel_scale 
+                #else:
+                #    scale = self.bck_vel_scale
+                
+                scale = 1
+                cmd = Helm()
+                cmd.yaw_rate = data.axes[0] * self.rotation_scale 
+                cmd.thrust_pct = data.axes[1] * scale
+                self.helm_pub.publish(cmd)
         else: # command open loop thrusters on a wrench message
             scale = 0
             if data.buttons[self.deadman_button] == 1:
@@ -75,7 +118,7 @@ class Teleop:
                 else:
                     scale = self.bck_force_scale
                 cmd = Wrench()
-                cmd.torque.z = -data.axes[0] * self.torque_scale
+                cmd.torque.z = data.axes[0] * self.torque_scale
                 cmd.force.x = data.axes[1]  * scale
                 self.wrench_pub.publish(cmd)
 
